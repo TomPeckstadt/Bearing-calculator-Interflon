0604517c5ed61e1cde097cef60d7429091b0d775
TomPeckstadt <tom.peckstadt@gmail.com>
2026-08-31 18:36:25 +0200

    Update app.js via Antigravity

--- app.js
+++ app.js
@@ -10489,7 +10489,7 @@ function getSurveyUrl() {
   const clientEmail = localStorage.getItem("client_email") || "";
 
   let params = new URLSearchParams();
-  params.set("v", "20260831_1542");
+  params.set("v", "20260831_1804");
   if (typeof currentLang !== "undefined" && currentLang) params.set("lang", currentLang);
   if (opEmail) params.set("contact", opEmail);
   if (clientCompany) params.set("company", clientCompany);
